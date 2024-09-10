9af1f54eeaedd7a002b6e541e87ebf7b81f65d2b
71904_Prafull <83003290+PrafulNimje@users.noreply.github.com>
2024-09-10 14:21:47 +0530

    new

--- functions/table.c
+++ functions/table.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-void charline(int, char);
+int charline(int, char);
 
 int main(){
 
-    int len;
+    int len,cnt;
     char ch;
 
     printf("Enter the length of line: ");
@@ -11,28 +11,32 @@ int main(){
     printf("Enter character: ");
     scanf(" %c",&ch);
     
-    charline(len,ch);
+    cnt= charline(len,ch);
     printf("\nData type \t size in bytes\n");
 
-    charline(len,ch);
+    cnt=charline(len,ch);
     printf("\nInteger \t %d\n",sizeof(int));
 
-    charline(len,ch);
+    cnt=charline(len,ch);
     printf("\nCharacter \t %d\n",sizeof(char));
 
-    charline(len,ch);
+    cnt=charline(len,ch);
     printf("\nFloat \t\t %d\n",sizeof(float));
-    charline(len,ch);
+    cnt=charline(len,ch);
+   
+    printf("\nFunction is called %d times\n",cnt);
 
     return 0;
-
-
 }
 
 
-void charline(int len, char ch){
+int charline(int len, char ch){
     int i;
+    static int count;
     for(i =1; i<=len;i++){
         printf(" %c",ch);
     }
+    count++;
+    return count;
+    
 }
